04e24e56b979556e9effa3bd264ca27e8f45222d
Smile Thanapattheerakul <smiile@Smiles-MacBook-Pro.local>
2020-04-26 01:01:08 -0400

    Add Fb and Messenger to Share contextMenus

--- background.js
+++ background.js
@@ -5,6 +5,28 @@ var contextMenuItem = {
     "contexts": ["selection"]
 };
 
+var fbContextMenu = {
+    "id": "fb",
+    "title": "Facebook",
+    "contexts": ["selection"],
+    "parentId": "selectionContent",
+    "onclick": function (tab) {
+        let selection = parseContent(tab);
+        shareToFB(selection);
+    }
+}
+
+var messengerContextMenu = {
+    "id": "messenger",
+    "title": "Messenger",
+    "contexts": ["selection"],
+    "parentId": "selectionContent",
+    "onclick": function (tab) {
+        let selection = parseContent(tab);
+        sendToMessenger(selection);
+    }
+}
+
 /**
  * Get the URL of current page
  * @param {tab} tab 
@@ -27,6 +49,33 @@ function parseContent(tab) {
     return selection;
 }
 
+function shareToFB(selection) {
+    FB.ui({
+        display: 'popup',
+        method: 'share',
+        quote: selection.text,
+        href: selection.url,
+    });
+}
+
+function sendToMessenger(selection) {
+    FB.ui({
+        method: 'send',
+        display: 'popup',
+        link: selection.url
+    });
+}
+
 chrome.browserAction.onClicked.addListener(getURL);
 chrome.contextMenus.create(contextMenuItem);
-chrome.contextMenus.onClicked.addListener(parseContent);
+chrome.contextMenus.create(fbContextMenu);
+chrome.contextMenus.create(messengerContextMenu);
+// chrome.contextMenus.onClicked.addListener(parseContent);
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '235948987509703',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v6.0'
+    });
+};
